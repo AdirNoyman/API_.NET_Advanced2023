eed042b567ef74cce6d82cc346c4f7716bffc7eb
AdirNoyman <ofahol2007@hotmail.com>
2023-01-26 21:59:50 +0200

    first commit

---
+++ CourseLibrary.API/Models/CourseForUpdateDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Models
+{
+    public class CourseForUpdateDto
+    {
+
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+
+    }
+}
---
+++ CourseLibrary.API/StartupHelperExtensions.cs
@@ -0,0 +1,86 @@
+﻿using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseLibrary.API;
+
+internal static class StartupHelperExtensions
+{
+    // Add services to the container
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers(configure =>
+        {
+            // Do not accept unsupported media types requests (according to our API definition)
+            configure.ReturnHttpNotAcceptable = true;
+
+        }
+        // Add support for XML media type response
+        ).AddXmlDataContractSerializerFormatters();
+
+        builder.Services.AddScoped<ICourseLibraryRepository,
+            CourseLibraryRepository>();
+
+        builder.Services.AddDbContext<CourseLibraryContext>(options =>
+        {
+            options.UseSqlite(@"Data Source=library.db");
+        });
+
+        builder.Services.AddAutoMapper(
+            AppDomain.CurrentDomain.GetAssemblies());
+
+        return builder.Build();
+    }
+
+    // Configure the request/response pipelien
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            // Not show stack trace and full error page
+            app.UseExceptionHandler(appBuilder =>
+            {
+                appBuilder.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    await context.Response.WriteAsync(
+                        "An unexpetced fault happend.Try again later 🤨"
+                    );
+                }
+                );
+            }
+            );
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+
+    public static async Task ResetDatabaseAsync(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            try
+            {
+                var context = scope.ServiceProvider.GetService<CourseLibraryContext>();
+                if (context != null)
+                {
+                    await context.Database.EnsureDeletedAsync();
+                    await context.Database.MigrateAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger>();
+                logger.LogError(ex, "An error occurred while migrating the database.");
+            }
+        }
+    }
+}
